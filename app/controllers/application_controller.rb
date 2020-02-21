1bbe0b73527f633f6444783c93468a6b098e4516
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-02-21 16:19:30 +0800

    Add redirect to dashboard after user sign in or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,20 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # If you have extra params to permit, append them to the sanitizer.
   def configure_permitted_parameters
     added_attrs = [:email, :password, :password_confirmation, :remember_me, :name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  # The path used after sign up.
+  def after_sign_up_path_for(resource_or_scope)
+    dashboard_root_path
+  end
+
+  # The path used after sign in.
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,6 @@ Rails.application.routes.draw do
   devise_for :participants
   devise_for :sponsors
   namespace :dashboard do
-    authenticated :participant do
-        resources :subjects, module: "participant", only: [:index]
-    end
-
-    authenticated :sponsor do
-        resources :subjects, module: "sponsor"
-    end
-
     root to: 'dashboard#index'
   end
 end
